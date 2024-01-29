a701b0ffa4906f5e432d7d9a457353e0497a5ef1
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-01-29 14:04:27 +0200

    feat: done cli env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    const parsedVars = {};
+
+    for (const key in process.env) {
+      if (key.startsWith('RSS_')) {
+        const varName = key;
+        const varValue = process.env[key];
+        parsedVars[varName] = varValue;
+      }
+    }
+  
+    for (const [varName, varValue] of Object.entries(parsedVars)) {
+      console.log(`${varName}=${varValue}`);
+    }
 };
 
 parseEnv();
