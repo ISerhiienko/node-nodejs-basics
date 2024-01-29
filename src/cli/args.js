e87225b3a52de8fb044d9172cf142263b4001c03
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-01-29 13:26:46 +0200

    feat: done cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,28 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const [, , ...commandLineArgs] = process.argv;
+
+    const parsedArgs = parseCommandLineArgs(commandLineArgs);
+    printParsedArgs(parsedArgs);
+  };
+  
+  const parseCommandLineArgs = (args) => {
+    const parsedArgs = {};
+  
+    for (let i = 0; i < args.length; i += 2) {
+      const argName = cleanArgName(args[i]);
+      const argValue = args[i + 1];
+      parsedArgs[argName] = argValue;
+    }
+  
+    return parsedArgs;
+  };
+  
+  const cleanArgName = (arg) => arg.replace(/^--/, '');
+  
+  const printParsedArgs = (parsedArgs) => {
+    for (const [argName, argValue] of Object.entries(parsedArgs)) {
+      console.log(`${argName} is ${argValue}`);
+    }
+  };
 
 parseArgs();
