47b56412e5952eac5e5dbcc7a58e70dcd4d3f4f7
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-01-29 17:07:58 +0200

    feat: done compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const fileName = "fileToCompress.txt";
+  const outputFileName = "archive.gz";
+
+  const currentModulePath = dirname(fileURLToPath(import.meta.url));
+
+  const filePath = path.resolve(currentModulePath, "files", fileName);
+  const filePathOutput = path.resolve(
+    currentModulePath,
+    "files",
+    outputFileName
+  );
+
+  const readStream = createReadStream(filePath);
+  const gzip = createGzip();
+  const writeStream = createWriteStream(filePathOutput);
+
+  try {
+    await pipeline(readStream, gzip, writeStream);
+    console.log("Compression complete.");
+  } catch (error) {
+    console.error("Compression failed:", error);
+  }
 };
 
 await compress();
