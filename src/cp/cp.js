89c68734033bf012c495ca45c5a6ae0468c0f1d9
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-01-29 21:36:51 +0200

    feat: done child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+
+  const childModule = path.resolve(currentDir, "files", "script.js");
+  const childProcess = spawn("node", [childModule, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["node", "js"]);
